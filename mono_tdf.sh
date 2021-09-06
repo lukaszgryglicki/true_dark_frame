0a613f47195670f7cfc0bf77476a0780e6c83ffc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-09-06 11:30:38 +0200

    Fix the script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- mono_tdf.sh
+++ mono_tdf.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-mkdir ./jpeg 2>/dev/null
 tiff_file=`echo $1 | cut -f 1 -d .`.tiff
 tiff2_file="out_$tiff_file"
 tmp=`echo $1 | cut -f 1 -d .`.jpeg
 jpeg_file="./tdf_$tmp"
-dcraw_mono.sh "$1" 1>/dev/null 2&>1
-tdf "$tiff_file" 1>/dev/null 2&>1
-convert_mono.sh "$tiff2_file" "$jpeg_file" 1>/dev/null 2&>1
+dcraw_mono.sh "$1" 1>/dev/null 2>&1
+tdf "$tiff_file" 1>/dev/null 2>&1
+convert_mono.sh "$tiff2_file" "$jpeg_file" 1>/dev/null 2>&1
 rm -f "$tiff_file" "$tiff2_file"
 ls -l "$jpeg_file"
