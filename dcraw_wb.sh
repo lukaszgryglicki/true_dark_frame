41b9d6d8f88bde6d543c43e969f700e8c908ae33
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-09-08 19:47:13 +0200

    One more update
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- dcraw_wb.sh
+++ dcraw_wb.sh
@@ -6,6 +6,7 @@ function cleanup {
 }
 trap cleanup EXIT
 #dcraw -v -H 0 -A 200 200 5600 3600 -T -q 3 "$1" || exit 1
-dcraw -v -H 0 -a -T -q 3 "$1" || exit 1
+# -H 1, 5+ looks best for me
+dcraw -v -H 1 -a -T -q 3 "$1" || exit 1
 convert "$tiff" -quality 98% "$jpeg" || exit 2
 RR=1 RG=0 RB=0 GR=0 GG=1 GB=0 BR=0 BG=0 BB=1 RLO=.2 RHI=.2 GLO=.2 GHI=.2 BLO=.2 BHI=.2 NA=1 Q=95 jpeg "$jpeg" || exit 3
