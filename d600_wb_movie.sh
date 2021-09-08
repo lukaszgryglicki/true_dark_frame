ad55f1bab6388b58ab1fc0c90641aeafdc9cc9dc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-09-08 21:43:51 +0200

    Script updates
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- d600_wb_movie.sh
+++ d600_wb_movie.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 # FPS=60
+# JQUAL=80
 if [ -z "${FPS}" ]
 then
-  FPS="29.970"
+  result=`ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "${1}"`
+  FPS=`echo "scale=3; ${result}" | bc`
+  if [ -z "${FPS}" ]
+  then
+    FPS="29.970"
+  else
+    echo "detected fps=${FPS}"
+  fi
 fi
+if [ -z "${JQUAL}" ]
+then
+  JQUAL=99
+fi
+echo "jpeg quality: ${JQUAL}"
 root=`echo $1 | cut -f 1 -d .`
-#ffmpeg -i "$1" -vcodec h264 -mbd 2 -preset slower -crf 19 -acodec aac -ac 1 -ar 22050 -f mp4 "$out"
-#ffmpeg -i _FSC3138.MOV '%04d.png'
-ffmpeg -i "$1" "${root}_%06d.png" || exit 1
-ffmpeg -i "$1" -vn -acodec aac -ac 2 -ar 48000 -f mp4 "${root}.aac" || exit 2
+ffmpeg -i "$1" -qmin 1 -qmax 18 "${root}_%06d.png" || exit 1
+ffmpeg -i "$1" -vn -acodec aac -ac 2 -ar 48000 -f mp4 -y "${root}.aac" || exit 2
 for f in ${root}_*.png
 do
   if [ -z "$size" ]
   then
     size=`convert "$f" -format "%wx%h" info:`
   fi
+  jf=`echo $f | cut -f 1 -d .`.jpeg
   convert "${f}" \
   \( -clone 0 -resize 1x1! -resize $size! -modulate 100,100,0 \) \
   \( -clone 0 -fill "gray(50%)" -colorize 100 \) \
-  -compose colorize -composite "wb_${f}" || exit 3
-  rm -f "${f}" || exit 4
+  -compose colorize -composite -quality "${JQUAL} "${jf}" || exit 3
+  Q="${JQUAL}" jpeg.sh "${jf}" || exit 4
+  rm -f "${f}" "${jf}" || exit 5
 done
-ffmpeg -i "wb_${root}_%06d.png" -i "${root}.aac" -r "${FPS}" -s "${size}" -vcodec h264 -mbd 2 -preset slower -crf 19 -shortest "${root}.mp4" || exit 5
-rm -f "wb_${root}_%06d.png" || exit 6
+ffmpeg -i "co_${root}_%06d.jpeg" -i "${root}.aac" -r "${FPS}" -s "${size}" -vcodec h264 -mbd 2 -preset slower -crf 18 -shortest "${root}.mp4" || exit 6
+rm -f co_${root}_*.jpeg || exit 7
 echo "OK: ${root}.mp4"
