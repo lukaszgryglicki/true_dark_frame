0431308c9785db8937625371f0ca90626a241123
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-09-09 06:01:32 +0200

    Fix the script
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- d600_wb_movie.sh
+++ d600_wb_movie.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# FPS=60
-# JQUAL=80
+# FPS=29.97
+# JQUAL=90
+# IJQUAL=99
+# VQ=
 if [ -z "${FPS}" ]
 then
   result=`ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate "${1}"`
@@ -14,11 +16,20 @@ then
 fi
 if [ -z "${JQUAL}" ]
 then
-  JQUAL=99
+  JQUAL=90
 fi
-echo "jpeg quality: ${JQUAL}"
+if [ -z "${IJQUAL}" ]
+then
+  IJQUAL=99
+fi
+echo "jpeg quality: ${IJQUAL}/${JQUAL}"
+if [ -z "${VQ}" ]
+then
+  VQ=20
+fi
+echo "video quality: ${VQ}"
 root=`echo $1 | cut -f 1 -d .`
-ffmpeg -i "$1" -qmin 1 -qmax 18 "${root}_%06d.png" || exit 1
+ffmpeg -i "$1" -qmin 1 -qmax "${VQ}" "${root}_%06d.png" || exit 1
 ffmpeg -i "$1" -vn -acodec aac -ac 2 -ar 48000 -f mp4 -y "${root}.aac" || exit 2
 for f in ${root}_*.png
 do
@@ -30,10 +41,10 @@ do
   convert "${f}" \
   \( -clone 0 -resize 1x1! -resize $size! -modulate 100,100,0 \) \
   \( -clone 0 -fill "gray(50%)" -colorize 100 \) \
-  -compose colorize -composite -quality "${JQUAL}" "${jf}" || exit 3
+  -compose colorize -composite -quality "${IJQUAL}" "${jf}" || exit 3
   Q="${JQUAL}" jpeg.sh "${jf}" || exit 4
   rm -f "${f}" "${jf}" || exit 5
 done
-ffmpeg -i "co_${root}_%06d.jpeg" -i "${root}.aac" -r "${FPS}" -s "${size}" -vcodec h264 -mbd 2 -preset slower -crf 18 -shortest "${root}.mp4" || exit 6
+ffmpeg -framerate "${FPS}" -i "co_${root}_%06d.jpeg" -r "${FPS}" -i "${root}.aac" -s "${size}" -vcodec h264 -mbd 2 -preset slower -crf "${VQ}" -shortest -y "${root}.mp4" || exit 6
 rm -f co_${root}_*.jpeg || exit 7
 echo "OK: ${root}.mp4"
