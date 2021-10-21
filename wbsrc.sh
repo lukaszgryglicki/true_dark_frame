dba20d49d5c6b9130704308f423ff5a14238a172
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-10-21 11:44:01 +0200

    Support no LG tools
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- wbsrc.sh
+++ wbsrc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # WB=filename.NEF - white balance reference file
+# NOLG=1 - do not use lukaszgryglicki's "jpeg/jpegbw" tools - use ImageMagick's "convert" instead
 # MULT="R G B G" - manually specify white balance, example: 0.37033 1.0 0.525732 0.996594, overrides WB
 # DCARGS="-v -H 0" - manually specify dcraw args for conversion, default is "-H 1 -W -q 3 -6", other can be "-v -H 0 -q 0 -b 1.2 -g 2.1 4.2" for example
 # The most imporant dcraw param is: -H [0-9] Highlight mode (0=clip, 1=unclip, 2=blend, 3+=rebuild)
@@ -35,11 +36,20 @@ do
     echo "$0: skipping white balance source file: ${WB}"
     continue
   fi
+  echo "processing ${f}"
   tiff=`echo "${f}" | cut -f 1 -d .`.tiff
   jpeg=`echo "${f}" | cut -f 1 -d .`.jpeg
   dcraw ${DCARGS} -r ${MULT} -T "${f}" || exit 3
-  convert "${tiff}" -quality 99% "${jpeg}" || exit 4
-  rm -rf "${tiff}"
-  RR=1 RG=0 RB=0 GR=0 GG=1 GB=0 BR=0 BG=0 BB=1 RLO=.2 RHI=.2 GLO=.2 GHI=.2 BLO=.2 BHI=.2 NA=1 ACM=1 Q=95 jpeg "$jpeg" || exit 5
-  rm -rf "${jpeg}"
+  if [ -z "${NOLG}" ]
+  then
+    convert "${tiff}" -enhance -quality 99% "${jpeg}" || exit 4
+    rm -rf "${tiff}"
+    RR=1 RG=0 RB=0 GR=0 GG=1 GB=0 BR=0 BG=0 BB=1 RLO=3 RHI=.05 GLO=3 GHI=.05 BLO=3 BHI=.05 NA=1 ACM=1 Q=95 jpeg "${jpeg}" || exit 5
+    rm -rf "${jpeg}"
+    echo "saved co_${jpeg}"
+  else
+    convert "${tiff}" -auto-level -enhance -quality 95% "${jpeg}" || exit 6
+    rm -rf "${tiff}"
+    echo "saved ${jpeg}"
+  fi
 done
